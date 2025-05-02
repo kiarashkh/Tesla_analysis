7f270f0e75158a120a69241bc6d0022b5bbd0144
kiarashkh <kiarashk111@gmail.com>
2025-05-02 22:54:30 +0330

    adding the CI\CD implementation

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_pipeline():
     try:
-        subprocess.run(["python", "scripts/load_data.py"], check=True)
+        subprocess.run(["python", "/scripts/load_data.py"], check=True)
         print("Pipeline completed successfully")
     except subprocess.CalledProcessError as e:
         print(f"Pipeline failed: {e}")
